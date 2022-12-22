c407c58c48ea917aaa6390db6350d6592acd94d6
Cj Reganit <croixjamesss@Cjs-MacBook-Pro.local>
2022-12-21 21:39:04 -0800

    made game in index.js

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Your Code Here
+
+// get user name 
+let userName = window.prompt ("GIMME ME YOUR NAME NOW")
+    console.log (userName)
+
+//user score 
+let userScore = 0
+
+
+// prompt user with question 
+for (let i = 0; i < questions.length; i++)
+{
+    let question = questions[i] 
+    let userAnswer = window.prompt (question.text)
+    console.log (userAnswer)
+    if (userAnswer.toUpperCase() === question.correctAnswer) {
+        console.log ("Correct")
+ //log (Question ${i+1} is correct)
+ //adding 10 points to score 
+        userScore += 10 
+    }
+    else {
+        console.log ("Congratulations you played yourself ")
+//log (Question ${i+1} is incorrect)
+
+    }
+    console.log ("userScore", userScore)
+//console log shows only in console in developer tools 
+}
+
+// track user score 
+window.alert ("Good job loser:" + userScore)
+//window alert shows up on website
